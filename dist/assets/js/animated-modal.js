d27ab04b050351c96fc9207fd7ae7dd68cb75d48
Adam Jarvis <adam.jarvis@gmail.com>
2019-02-10 10:29:30 -0500

    chore: cleanup modal js

--- dist/assets/js/animated-modal.js
+++ dist/assets/js/animated-modal.js
@@ -90,7 +90,6 @@ function confirmModal(element, id) {
   closeModal(element, id, action);
 }
 
-
 // Closes the modal when passed an element id through data-attribute
 // Expects the use for `data-modal="id"` where id is the modal target
 function closeModal(element, id, action) {
@@ -132,58 +131,57 @@ function closeModal(element, id, action) {
   function exitAnimationCompleted() {
     modalTarget.classList.remove(exitAnimation, settings.activeClass);
   }
-
 }
 
 
-  //
-	// Utility Functions
-	//
+//
+// Utility Functions
+//
 
-  /**
-   * Merges two or more objects and returns a new object.
-   * mergeOptions(object, object) - Returns merged oject
-   */
-  function mergeOptions() {
-    var mergedOptions = {};
+/**
+ * Merges two or more objects and returns a new object.
+ * mergeOptions(object, object) - Returns merged oject
+ */
+function mergeOptions() {
+  var mergedOptions = {};
 
-    // Merge the object into the extended object (mergedOptions)
-    function merge(object) {
-      for (var prop in object) {
-        if (object.hasOwnProperty(prop)) {
-          mergedOptions[prop] = object[prop];
-        }
+  // Merge the object into the extended object (mergedOptions)
+  function merge(object) {
+    for (var prop in object) {
+      if (object.hasOwnProperty(prop)) {
+        mergedOptions[prop] = object[prop];
       }
-    };
-
-    // Loop through each object and merge
-    for (var i = 0; i < arguments.length; i++) {
-      merge(arguments[i]);
-    }
-
-    return mergedOptions;
-  };
-
-
-  /**
-   * Gets data options from data-modal-options attribute
-   * Converts options to an object
-   */
-  function getDataOptions(options) {
-    return (!options || typeof JSON.parse !== 'function') ? {} : JSON.parse(options);
-  };
-
-
-  /**
-   * Check for animation end
-   * Loop through prefixes for browser animationend variations
-   * Add event listener on passed element
-   */
-  var prefix = ["webkit", "moz", "MS", "o", ""];
-
-  function checkForAnimationEnd(element, type, callback) {
-    for (var p = 0; p < prefix.length; p++) {
-      if (!prefix[p]) type = type.toLowerCase();
-      element.addEventListener(prefix[p]+type, callback, false);
     }
   }
+
+  // Loop through each object and merge
+  for (var i = 0; i < arguments.length; i++) {
+    merge(arguments[i]);
+  }
+
+  return mergedOptions;
+}
+
+
+/**
+ * Gets data options from data-modal-options attribute
+ * Converts options to an object
+ */
+function getDataOptions(options) {
+  return (!options || typeof JSON.parse !== 'function') ? {} : JSON.parse(options);
+}
+
+
+/**
+ * Check for animation end
+ * Loop through prefixes for browser animationend variations
+ * Add event listener on passed element
+ */
+var prefix = ["webkit", "moz", "MS", "o", ""];
+
+function checkForAnimationEnd(element, type, callback) {
+  for (var p = 0; p < prefix.length; p++) {
+    if (!prefix[p]) type = type.toLowerCase();
+    element.addEventListener(prefix[p]+type, callback, false);
+  }
+}
